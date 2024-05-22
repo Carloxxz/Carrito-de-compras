283232fad10b8be079b9425de814e2380b9c4c70
Carloxxz <carloxmz@gmail.com>
2024-05-21 18:10:01 -0600

    - Agrega funcion para añadir productos al carrito
    - Agrega funcion para eliminar cosas del carrito
    
    - Añade el componente header a la App
    
    - Muestra cada guitarra y sus props

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from './data/db'
 
 export default function App() {
 
     // State
-    const [auth, setAuth] = useState(false)
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+
+
+    function addToCart(item) {
+        const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+
+        if (itemExist >= 0) {
+            const updatedCart = [...cart]
+            updatedCart[itemExist].quantity++
+            setCart(updatedCart)
+        } else {
+            item.quantity = 1
+            setCart([...cart, item])
+        }
+
+    }
+
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+    }
+
 
     return (
         <>
-            <Header />
+            <Header
+                cart={cart}
+                removeFromCart={removeFromCart}
+            />
 
             <main className="container-xl mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
+                    {
+                        data.map((guitar) => (
+                            <Guitar
+                                key={guitar.id}
+                                guitar={guitar}
+                                setCart={setCart}
+                                addToCart={addToCart}
+                            />
 
-                    <Guitar />
+                        ))
+                    }
 
                 </div>
             </main>
